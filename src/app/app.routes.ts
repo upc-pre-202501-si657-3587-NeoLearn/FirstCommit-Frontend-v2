e0cec9c59a5f7123c929c4f8252680551a3b5460
Nestor Velarde <u20211c221@upc.edu.pe>
2025-06-03 12:03:50 -0500

    feat: Add routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,23 @@
-import { Routes } from '@angular/router';
+import {RouterModule, Routes} from '@angular/router';
+import {NgModule} from '@angular/core';
+
 import { MainLayout } from './firstcommit/layout/main-layout/main-layout';
 import { Components } from './firstcommit/roadmap/components/components'
+import { NotfoundComponent } from './firstcommit/public/notfound.component/notfound.component';
+
 export const routes: Routes = [
   {
-    path: '',
-    component: MainLayout,
+    path: '', component: MainLayout,
     children: [
       { path: 'roadmap', component: Components },
+      { path: '**', component: NotfoundComponent }
     ]
   }
+
 ];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
